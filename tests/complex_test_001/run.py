ca57e8a8e18b9613314e299dcd13938d6e22242b
CaribouJohn <john@heath.mx>
2018-11-08 15:13:48 +0000

    define test

--- tests/complex_test_001/run.py
+++ tests/complex_test_001/run.py
@@ -1,30 +1,41 @@
+# Sample PySys testcase
+# Copyright (c) 2015-2016, 2018 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors. 
+# Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG 
+
 from pysys.constants import *
-from pysys.basetest import BaseTest
-from apama.docker.framework import DockerImage, DockerSecret, DockerService
-import shutil, time, binascii, os, subprocess
+from apama.basetest import ApamaBaseTest
+from apama.correlator import CorrelatorHelper
 
-class PySysTest(BaseTest):
+class PySysTest(ApamaBaseTest):
+
 	def execute(self):
-		APAMA_HOME = os.getenv('APAMA_HOME', '')
-		samples = os.getenv('APAMA_SAMPLES', '%s/samples' % APAMA_HOME)
-
-		self.sampleDir = samples+"/docker/applications/Secrets"
-
-		if PROJECT.CORRELATOR_IMAGE:
-			img = DockerImage.fromExisting(self, PROJECT.CORRELATOR_IMAGE)
-		else:
-			img = DockerImage.fromDockerfile(self, file=os.path.join(PROJECT.DOCKER_PRODUCT_HOME, "image/Dockerfile"), context=PROJECT.APAMA_SAG_HOME)
+		# create the correlator helper, start the correlator and attach an 
+		# engine_receive process listening to a test channel. The helper will 
+		# automatically get an available port that will be used for all 
+		# operations against it
+		correlator = CorrelatorHelper(self, name='testcorrelator')
+		correlator.start(logfile='testcorrelator.log')
+		receiveProcess = correlator.receive(filename='receive.evt', channels=['EchoChannel'], logChannels=True)
+		correlator.applicationEventLogging(enable=True)
 		
-		app = DockerImage.fromDockerfile(self, file=self.sampleDir+'/Dockerfile', context=self.sampleDir, buildArgs=['APAMA_IMAGE='+img.getName()])
+		# inject the simple.mon monitor (directory defaults to the testcase input)
+		correlator.injectEPL(filenames=['ComplexPlugin.mon'])
 		
-		secret = DockerSecret.fromContent(self, secretContents='CORRELATOR_NAME=MyCorrelatorName', name=DockerImage.generateUniqueName()+".properties")
-
-		service = DockerService.fromImage(app)
-
-		service.run(extraArgs=["--secret="+secret.getName()])
-		service.log('correlator.log')
-
-		self.waitForSignal(file="correlator.log.out", expr="Component ID:")
-
+		# wait for all events to be processed
+		correlator.flush()
+		
+		# wait until the receiver writes the expected events to disk
+		#self.waitForSignal('receive.evt', expr="SimpleEvent", condition=">=2")
+		
 	def validate(self):
-		self.assertGrep(file="correlator.log.out", expr="Component ID: MyCorrelatorName")
+		# look for log statements in the correlator log file
+		self.assertGrep('testcorrelator.log', expr=' (ERROR|FATAL) ', contains=False)
+		
+		#exprList = []
+		#exprList.append('Received simple event with message - This is the first simple event')
+		#exprList.append('Received simple event with message - This is the second simple event')
+		#self.assertOrderedGrep('testcorrelator.log', exprList=exprList)
+	
+		# check the received events against the reference
+		#self.assertDiff('receive.evt', 'ref_receive.evt')
+		
